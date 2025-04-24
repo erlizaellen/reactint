e285a55abf05a6c4b02f1c2653a49c54fc9f7149
erlizaellen <erlizaellen@gmail.com>
2025-04-24 11:50:09 -0300

    finalizada pagina de login

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -1,27 +1,81 @@
+
+import { useState } from "react"
 import imgAnimada from "../public/tech.svg"
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function App(){
+  //hooks 
+  const [user, setUser]= useState("")
+  const [pass, setPass]= useState("")
+ 
+
+ // função para usar no click do botao 
+ function clickLogin(){
+   
+    if(!user || !pass){
+      return toast.error("Preencha todos os campos")
+
+    }
+
+    if (user.length <8 || pass.length <8){
+      return toast.error("Senha Inválida")
+    }
+
+    
+
+    //requisiçao para o backend
+    console.log(user)
+    console.log(pass)
+    
+    
+    toast.success('Sucesso!')
+ }
+
   return (
     <div className="w-full h-screen flex bg-[#201B2C]">
-     <div className="esquerda w-[50%] h-full flex items-center justify-center flex-col bg-[#606066]" >
-      <h1 className="text-[30px] font-bold text-[#00FF88]">Entre para o nosso time</h1>
-      <img src={imgAnimada} alt="imagem-animada" width={550}/>
+      <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      theme="colored" 
+      />
+     <div className="esquerda w-[50%] h-full flex items-center justify-center flex-col bg-[#201B2C]" >
+      <h1 className="text-[30px] font-bold text-[#e0ac39]">Entre para o nosso time</h1>
+      <img src={imgAnimada} alt="imagem-animada" width={500}/>
      </div>
      <div className="direita w-[50%] h-full flex items-center justify-center ">
-        <div className="w-[50%] h-[350px] bg-[#5B3AC7FF] rounded-xl">
+        <div className="w-[50%] h-[350px] bg-[#ae47b8] rounded-xl">
           <div className="w-full h-[15%] flex items-center justify-center ">
-            <h1 className="text-[30px] text-[#db6c38] font-bold">LOGIN</h1>
+            <h1 className="text-[30px] text-[#f0f32a] font-bold">LOGIN</h1>
           </div>
-          <div className="w-full h-[70%] p-[20px]">
+          <div className="w-full h-[60%] p-[20px]">
             <label htmlFor="user" className="text-white ">Usuário</label>
-            <input type="text" id="user" className="w-full rounded-md p-[10px] bg-amber-400 text-white mb-4" />
+            <input 
+            onChange={
+              (event)=> {setUser(event.target.value)}
+
+            }
+            type="text" 
+            id="user" 
+            className="w-full rounded-md p-[10px] bg-amber-400 text-white mb-4" 
+            />
 
             <label htmlFor="pass" className="text-white ">Senha</label>
-            <input type="password" id="pass" className="w-full rounded-md p-[10px] bg-amber-400 text-white" />
+            <input
+              onChange={
+              (event) => {setPass(event.target.value)}
 
+            }
+            type="password"
+            id="pass" 
+            className="w-full rounded-md p-[10px] bg-amber-400 text-white" 
+              />
+            <div className="w-full flex items-center justify-center text-white pt-1"> <a href="">Esqueceu sua Senha?</a>
+            </div>
           </div>
-          <div className="w-full h-[15%] ">
-            <button className="w-full p-[10px] text-[20px] bg-amber-700 " ></button>
+          <div className="w-full h-[25%] p-[20px] flex items-center justify-center ">
+            <button 
+            onClick={clickLogin}
+            className="w-full h-[50px]  text-[20px] bg-[#ebbc21] rounded-lg text-[#eeebeb] font-bold" >Login</button>
           </div>
         </div>
      </div>
